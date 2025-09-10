cbad90590719b5df1f59f13fba8c39c287fc8178
carlos gomes barbosa <caina0201@gmail.com>
2025-09-10 10:02:02 -0300

    Delete Modulo 1/Condicionais/decrescente.py

--- Modulo 1/Condicionais/decrescente.py
+++
@@ -1,8 +0,0 @@
-#Faça um programa que peça uma nota, entre zero e dez. Mostre uma mensagem caso o valor seja inválido e continue pedindo até que o usuário informe um valor válido.
-
-while True:
-    nota = int(input("digite uma nota: "))
-    if nota >=0 and nota <=10:
-        break
-    print("valor invalido")
-
