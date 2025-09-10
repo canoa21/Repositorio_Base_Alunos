aa4690ba19d608ed65ae91cfe46e76b6383ec960
carlos gomes barbosa <caina0201@gmail.com>
2025-09-10 10:02:20 -0300

    Delete Modulo 1/Condicionais/html1.py

--- Modulo 1/Condicionais/html1.py
+++
@@ -1,9 +0,0 @@
-import requests, os, bs4
-url = 'http:// xkcd.com' #url inicial
-os.makedirs('xkcd', exist_ok = True) #armazena as tirnhas em ./xkdc
-while not url.endswith('#'):
-    #faz download da pagina.
-    print('downloading page %s...' % url)
-res = requests.get(url)
-res.raise_for_status()
-soup = bs4.BeautifulSoup(res.text)
