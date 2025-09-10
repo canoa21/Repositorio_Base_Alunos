34d9d4abe53451b6c81ff8812645b359ad58b5a0
carlos gomes barbosa <caina0201@gmail.com>
2025-09-10 10:12:22 -0300

    Add files via upload

---
+++ Modulo 1/Codigos Basicos/lista1.py
@@ -0,0 +1,15 @@
+#lista inicial
+nomes = ["joaquim", "maria","maria"]
+print ("lista inicial:", nomes)
+
+#adicionando elementos 
+nomes . append("carlos") #adiciona ao final 
+print("lista inicial:", nomes)
+
+
+nomes.insert(1, "fernanda") #insere "fernandada" na posição 1
+print("após insert:", nomes)
+
+#modificando elementos
+nomes [2] = "paulo" #modifica o elemnto no índicie 2
+print("após modificação:", nomes)
