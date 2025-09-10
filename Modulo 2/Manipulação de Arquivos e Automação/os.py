a4b8e5d288fe88230d248bb2e290c49b109ffbba
carlos gomes barbosa <caina0201@gmail.com>
2025-09-10 10:44:48 -0300

    Add files via upload

---
+++ Modulo 2/Manipulação de Arquivos e Automação/os.py
@@ -0,0 +1,20 @@
+import os
+
+coisa = os.path.abspath('C:\\Users\\Aluno_Programador\\Downloads')
+coisa2 = os.path.dirname('C:\\Users\\Aluno_Programador\\Downloads')
+coisa3 = os.path.basename('C:\\Users\\Aluno_Programador\\Downloads')
+coisa4 = os.path.getsize('C:\\Users\\Aluno_Programador\\Downloads')
+coisa5 = os.listdir('C:\\Users\\Aluno_Programador\\Downloads')
+
+
+print(coisa)
+print()
+print(coisa2)
+print()
+print(coisa3)
+print()
+print(coisa4)
+print()
+print(coisa5)
+
+    
