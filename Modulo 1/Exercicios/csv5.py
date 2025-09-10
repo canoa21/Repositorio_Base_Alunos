9807a78318c2e0865b0a4b8d4241091727239134
carlos gomes barbosa <caina0201@gmail.com>
2025-09-10 10:41:02 -0300

    Delete Modulo 1/Exercicios/csv5.py

--- Modulo 1/Exercicios/csv5.py
+++
@@ -1,9 +0,0 @@
-nome = input("digite seu nome: ")
-email = int(input("digite deu email: "))
-telefone = int(input("digite seu numero de telefone "))
-
-with open('clientes.csv', 'a', newline='', encoding='utf-8') as arquivo:
-    escritor = csv.writer(arquivo)
-    with open('clientes.csv', 'a', newline='', encoding='utf-8') as arquivo:
-    
-    escritor = csv.writer(arquivo)
